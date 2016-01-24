fe5e59148f0b02cc2ec3e479fc70a15cfdce70ae
Michael Mao <michael340@gmail.com>
2016-01-23 19:37:39 -0500

    Fix updated variable names.

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ def main():
     t = Thermostat()
 
     # get and set current temperature
-    current_temp = int(input('Enter current temperature: '))
+    current_temperature = int(input('Enter current temperature: '))
     try:
-        t.current_temp = current_temp
+        t.current_temperature = current_temperature
     except errors.ValidationErrorBase as e:
         print(e)
 
     # get and set user user temperature range
-    user_temp_low = int(input('Enter lower bound: '))
-    user_temp_high = int(input('Enter higher bound: '))
+    temp_low = int(input('Enter lower bound: '))
+    temp_high = int(input('Enter higher bound: '))
     try:
-        t.user_temp_range = (user_temp_low, user_temp_high)
+        t.temperature_range = (temp_low, temp_high)
     except errors.TemperatureValidationError as e:
         print(e)
-
-    t.run()
+    else:
+        t.run()
 
 
 if __name__ == "__main__":
