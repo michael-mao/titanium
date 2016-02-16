eb104c95d0c0e6059b311cd2aa4c1c74c47bf2c9
Michael Mao <michael340@gmail.com>
2016-02-15 22:13:20 -0500

    Add decision matrix data structure and evaluation. * Preliminary implementation, constant factors may need to be tweaked * See code for details

--- thermostat/thermostat.py
+++ thermostat/thermostat.py
@@ -3,8 +3,10 @@
 import time
 
 from logging import getLogger
+from functools import reduce
 from enum import Enum, unique
-from . import errors, utils, weather
+
+from . import errors, utils, weather, sensor
 
 
 @unique
@@ -13,16 +15,35 @@ class State(Enum):
     HEAT = 1
     COOL = 2
 
+    def __str__(self):
+        return self.name
+
 
 class Thermostat(metaclass=utils.Singleton):
+    # bounds for input range
     MIN_TEMPERATURE = 0
     MAX_TEMPERATURE = 35
+
+    # decision matrix thresholds, within range(-1, 1)
+    # -1              COOL_THRESHOLD      HEAT_THRESHOLD          1
+    # --------COOL--------|--------OFF--------|--------HEAT--------
+    DM_HEAT_THRESHOLD = 0.5
+    DM_COOL_THRESHOLD = -0.5
+
+    # decision matrix parameters and their weighting, weightings must sum to 1
+    DM_WEIGHTINGS = {
+        'internal_temperature': 0.4,
+        'external_temperature': 0.2,
+        'history_temperature': 0.2,
+        'energy_price': 0.2,
+    }
 
     def __init__(self):
         self._settings = utils.init_settings()
         self._current_temperature = 0.0
         self._temperature_range = (0.0, 0.0)
         self._on_rpi = utils.on_rpi()
+        self.temperature_increment = 0.5
         self.update_interval = 5
         self.state = State.OFF
         self.logger = getLogger('app.thermostat')
@@ -32,6 +53,11 @@ class Thermostat(metaclass=utils.Singleton):
 
         Main event loop is here.
         """
+        # initialize sensor
+        if self.on_rpi:
+            sensor.init_sensor()
+            self.current_temperature = sensor.read_temperature()
+
         # daemon thread to fetch weather data
         weather_thread = weather.WeatherAPI(
             self._settings['temperature_unit'],
@@ -45,16 +71,84 @@ class Thermostat(metaclass=utils.Singleton):
             time.sleep(self.update_interval)
 
     def update_state(self):
+        """ Decision maker.
+
+        Gathers input and data to update thermostat state.
+        """
+        self.logger.debug('thermostat state is {0}'.format(self.state))
+        if self.on_rpi:
+            self.current_temperature = sensor.read_temperature()
+
         low, high = self.temperature_range
-        if self.current_temperature < low:
-            self.logger.debug('Thermostat is heating.')
+        if self.current_temperature < (low - self.temperature_increment):
             self.state = State.HEAT
-        elif self.current_temperature > high:
-            self.logger.debug('Thermostat is cooling.')
+        elif self.current_temperature > (high + self.temperature_increment):
             self.state = State.COOL
         else:
-            self.logger.debug('Thermostat is inactive.')
-            self.state = State.OFF
+            # within range, use decision matrix
+            params_list = list()
+            params_list.append(('internal_temperature', self.temperature_range_equilibrium - self.current_temperature))
+            # TODO: weather, history, price
+            matrix = self.build_decision_matrix(params_list)
+            self.state = self.evaluate_decision_matrix(matrix)
+
+        self.logger.debug('thermostat state updated to {0}'.format(self.state))
+
+    @classmethod
+    def build_decision_matrix(cls, params_list):
+        """ Creates decision matrix data structure.
+
+        Matches passed in parameter and rating tuple to the respective weighting in DM_WEIGHTINGS.
+        Parameters that do not exist in DM_WEIGHTINGS are ignored. Conversely, if no matching tuple is passed in for
+        a key in DM_WEIGHTINGS, it will not be included in the final matrix (other parameter weightings will be
+        recalculated accordingly).
+
+        :param params: list of tuples, (parameter_name, rating)
+            - parameter_name must match a key in DM_WEIGHTINGS
+        :return: decision matrix, dictionary of the form { parameter: (weight, rating), ... }
+        """
+        matrix = {}
+        total_weight = 0.0
+        for parameter, rating in params_list:
+            weight = cls.DM_WEIGHTINGS.get(parameter)
+            if weight is not None:
+                matrix[parameter] = (weight, rating)
+                total_weight += weight
+
+        # if there are missing parameter ratings, recalculate weightings
+        if len(matrix) != len(cls.DM_WEIGHTINGS):
+            for key, value in matrix.items():
+                weight, rating = value
+                matrix[key] = (weight/total_weight, rating)
+
+        return matrix
+
+    @classmethod
+    def evaluate_decision_matrix(cls, matrix):
+        """ Evaluates decision matrix.
+
+        Calculates the total score based on weighting and rating of each parameter.
+        Determines new thermostat state based on total score and preset thresholds.
+
+        Normalizes ratings to be within range(-1, 1) before multiplying weight.
+
+        :param matrix: decision matrix
+        :return: new thermostat state
+        """
+        total_score = 0.0
+        total_rating = reduce(lambda x, y: x+y, [value[1] for value in matrix.values()])
+
+        for key, value in matrix.items():
+            score = reduce(lambda weight, rating: weight*(rating/total_rating), value)
+            total_score += score
+
+        new_state = State.OFF
+        if total_score > cls.DM_HEAT_THRESHOLD:
+            new_state = State.HEAT
+        elif total_score < cls.DM_COOL_THRESHOLD:
+            new_state = State.COOL
+        print('Total score is {}'.format(total_score))
+        return new_state
 
     @classmethod
     def validate_temperature(cls, value):
@@ -62,6 +156,10 @@ class Thermostat(metaclass=utils.Singleton):
             raise errors.TemperatureValidationError('Temperature cannot be below {}'.format(cls.MIN_TEMPERATURE))
         if value > cls.MAX_TEMPERATURE:
             raise errors.TemperatureValidationError('Temperature cannot be above {}'.format(cls.MAX_TEMPERATURE))
+
+    @property
+    def on_rpi(self):
+        return self._on_rpi
 
     @property
     def is_active(self):
@@ -73,7 +171,6 @@ class Thermostat(metaclass=utils.Singleton):
 
     @current_temperature.setter
     def current_temperature(self, value):
-        self.validate_temperature(value)
         self._current_temperature = value
 
     @property
@@ -88,3 +185,10 @@ class Thermostat(metaclass=utils.Singleton):
         self.validate_temperature(low)
         self.validate_temperature(high)
         self._temperature_range = t_range
+
+    @property
+    def temperature_range_equilibrium(self):
+        # biased towards range ceiling
+        low, high = self.temperature_range
+        difference = high - low
+        return (difference / 4) + difference
