9f9ada06e730c24fafcb0e27d09861bd208d78dc
Michael Mao <michael340@gmail.com>
2016-03-04 16:27:08 -0500

    Rearrange middleware, add error handler.

--- webapp/server.js
+++ webapp/server.js
@@ -14,67 +14,24 @@ var app = koa();
 
 app.use(bodyParser());
 
-router.get('/ping', function*() {
-  this.body = Date();
+// error handler
+app.use(function*(next) {
+  try {
+    yield next;
+  } catch(error) {
+    this.status = error.status || 500;
+    this.body = {
+      message: error.message || 'Server Error'
+    };
+    this.app.emit('error', error, this);
+  }
 });
 
-router
-  .post('/authenticate', function*() {
-    var data = this.request.body;
-    var user, hash, salt;
-
-    this.status = 401;
-    if(data.email && data.password) {
-      user = yield db.getUser(data.email);
-      if(user) {
-        salt = utils.extractSalt(user.password);
-        hash = yield utils.hashPassword(data.password, salt);
-        if(utils.appendSalt(hash, salt) === user.password) {
-          this.status = 201;
-        }
-      }
-    }
-  });
-
-router
-  .get('/api/user', function*() {
-    var query = this.request.query;
-    var user;
-
-    this.status = 404;
-    if(query.email) {
-      user = yield db.getUser(decodeURIComponent(query.email));
-      if(user) {
-        this.status = 200;
-        this.body = {
-          email: user.email
-        };
-      }
-    }
-  })
-  .post('/api/user', function*() {
-    var data = this.request.body;
-    var user, hash, salt;
-
-    if(data.email && data.password) {
-      salt = utils.generateSalt();
-      hash = yield utils.hashPassword(data.password, salt);
-      user = yield db.insertUser(data.email, utils.appendSalt(hash, salt));
-      this.body = {
-        email: user.email
-      };
-    } else {
-      this.status = 400;
-    }
-  })
-  .put('/api/user', function*() {
-
-  });
-
-app.use(router.routes());
-
-app.use(function*() {
-  if (yield send(this, this.path, config.SEND_OPTIONS)) {
+app.use(function*(next) {
+  if (this.path.indexOf('/api') !== -1) {
+    // api call
+    yield next;
+  } else if (yield send(this, this.path, config.SEND_OPTIONS)) {
     // file exists and request successfully served so do nothing
     return;
   } else if (this.path.indexOf('.') !== -1) {
@@ -86,6 +43,74 @@ app.use(function*() {
     yield send(this, '/index.html', config.SEND_OPTIONS);
   }
 });
+
+router.get('/ping', function*() {
+  this.body = Date();
+});
+
+router
+  .post('/api/authenticate', function*() {
+    var data = this.request.body;
+    var user, hash, salt;
+
+    if(data.email && data.password) {
+      user = yield db.getUser(data.email);
+      if(user) {
+        salt = utils.extractSalt(user.password);
+        hash = yield utils.hashPassword(data.password, salt);
+        if(utils.appendSalt(hash, salt) === user.password) {
+          this.status = 201;
+          return;
+        }
+      }
+      this.throw('Incorrect email and/or password', 401);
+    }
+    this.throw('Missing fields', 400);
+  });
+
+router
+  .get('/api/user', function*() {
+    var query = this.request.query;
+    var user;
+
+    if(query.email) {
+      user = yield db.getUser(decodeURIComponent(query.email));
+      if(user) {
+        this.body = {
+          email: user.email
+        };
+        return;
+      }
+      this.throw('User not found', 404);
+    }
+    this.throw('Invalid query', 400);
+  })
+  .post('/api/user', function*() {
+    var data = this.request.body;
+    var user, hash, salt;
+
+    if(data.email && data.password) {
+      salt = utils.generateSalt();
+      hash = yield utils.hashPassword(data.password, salt);
+      user = yield db.getUser(data.email);
+      if(!user) {
+        user = yield db.insertUser(data.email, utils.appendSalt(hash, salt));
+        this.body = {
+          email: user.email
+        };
+        this.status = 201;
+        return;
+      }
+      this.throw('Email already in use', 400);
+    }
+    this.throw('Missing fields', 400);
+  })
+  .put('/api/user', function*() {
+
+  });
+
+app.use(router.routes());
+
 
 app.listen(config.PORT);
 console.log('listening on port ' + config.PORT);
