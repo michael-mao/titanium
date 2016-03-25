6427149772fde68ee65bc03c4c1da5028f79264d
Michael Mao <michael340@gmail.com>
2016-03-25 01:05:36 -0400

    Update new gui to display dynamic values. * Update weather icons * Implement power toggle

--- thermostat/gui.py
+++ thermostat/gui.py
@@ -9,19 +9,18 @@ from logging import getLogger
 
 from . import utils, errors, config
 
+
 class GUI(metaclass=utils.Singleton):
-    HeatOrCool = 'h' # IF HEATING, SET TO 'h', ELSE WILL DISPLAY COOLING. SET ON LINE 154
-    External_Temp = 99
-    External_Temp_Icon = "sun" # OPTIONS are sun,rain,snow,wind. SET ON LINE 159
-    
+
     WIDTH = 480
     HEIGHT = 320
     SIZE = (WIDTH, HEIGHT)
     DEFAULT_FONT = 'roboto'
-    BACKGROUND_COLOUR = (255,255,255)
+    BACKGROUND_COLOUR = (255, 255, 255)
+    TEMP_TEXT_COLOUR = (0, 204, 0)
     BLACK = (0, 0, 0)
-    heating_color = 255,0,0 
-    cooling_color = 0,0,255
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
 
     def __init__(self, t):
         pygame.init()
@@ -37,38 +36,39 @@ class GUI(metaclass=utils.Singleton):
         self.fonts['current_temperature'] = pygame.font.SysFont(self.DEFAULT_FONT, 80)
         self.fonts['temperature_range'] = pygame.font.SysFont(self.DEFAULT_FONT, 45)
         self.fonts['settings'] = pygame.font.SysFont(self.DEFAULT_FONT, 20)
-        self.fonts['heat_or_cool'] = pygame.font.SysFont(self.DEFAULT_FONT, 50) 
-        self.fonts['external_temperature'] = pygame.font.SysFont(self.DEFAULT_FONT, 40)        
+        self.fonts['mode'] = pygame.font.SysFont(self.DEFAULT_FONT, 30)
+        self.fonts['external_temperature'] = pygame.font.SysFont(self.DEFAULT_FONT, 40)
+
         # load images
         self.images['up_arrow'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'up.png'))
         self.images['down_arrow'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'down.png'))
         self.images['settings_menu'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'gear.png'))
         self.images['settings_title'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'SettingsHeader.png'))
         self.images['back_button'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'back.png'))
-        self.images['off_button'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'off.png'))
+        self.images['power_off'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'power_off.png'))
+        self.images['power_on'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'power_on.png'))
+        self.images['clouds'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'clouds.png'))
         self.images['rain'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'rain.png'))
-        self.images['sun'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'sun.png'))
+        self.images['clear'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'sun.png'))
         self.images['snow'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'snow.png'))
-        self.images['wind'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'wind.png'))
+        self.images['mist'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'mist.png'))
+        self.images['thunderstorm'] = pygame.image.load(os.path.join(config.ASSETS_DIR, 'thunder.png'))
 
         # image positions
-        self.positions['current_temperature'] = pygame.Rect(187, 96,120,80)
-        self.positions['low_temp'] = pygame.Rect(66, 106, 70,45)
-        self.positions['high_temp'] = pygame.Rect(357, 106, 70,45)
-        self.positions['low_temp_up'] = pygame.Rect(64,32,64,60)
-        self.positions['low_temp_down'] = pygame.Rect(64,140,64,60)
-        self.positions['high_temp_up'] = pygame.Rect(352,32,64,60)
-        self.positions['high_temp_down'] = pygame.Rect(352,140,64,60)
-        self.positions['settings_menu'] = pygame.Rect(405,250,45,4500)
-        self.positions['settings_title'] = pygame.Rect(154,0,1,1)
-        self.positions['back_button'] = pygame.Rect(168,256,150,35)
-        self.positions['off_button'] = pygame.Rect(18,250,64,64)
-        self.positions['rain'] = pygame.Rect(250, 172,45,45)
-        self.positions['sun'] = pygame.Rect(250, 172,45,45)
-        self.positions['snow'] = pygame.Rect(250, 172,45,45)        
-        self.positions['wind'] = pygame.Rect(250, 172,45,45)
-        self.positions['heating'] = pygame.Rect(165,15,50,120)
-        self.positions['cooling'] = pygame.Rect(165,15,50,120)        
+        self.positions['current_temperature'] = pygame.Rect(190, 96, 120, 80)
+        self.positions['low_temp'] = pygame.Rect(66, 106, 70, 45)
+        self.positions['high_temp'] = pygame.Rect(357, 106, 70, 45)
+        self.positions['low_temp_up'] = pygame.Rect(64, 32, 64, 60)
+        self.positions['low_temp_down'] = pygame.Rect(64, 140, 64, 60)
+        self.positions['high_temp_up'] = pygame.Rect(352, 32, 64, 60)
+        self.positions['high_temp_down'] = pygame.Rect(352, 140, 64, 60)
+        self.positions['settings_menu'] = pygame.Rect(405, 250, 45, 4500)
+        self.positions['settings_title'] = pygame.Rect(154, 0, 1, 1)
+        self.positions['back_button'] = pygame.Rect(168, 256, 150, 35)
+        self.positions['power_toggle'] = pygame.Rect(18, 250, 50, 50)
+        self.positions['external_temp'] = pygame.Rect(185, 180, 60, 50)
+        self.positions['weather_icon'] = pygame.Rect(250, 172, 50, 50)
+        self.positions['mode'] = pygame.Rect(185, 280, 160, 40)
 
     def run(self):
         self.thermostat.start()
@@ -81,6 +81,8 @@ class GUI(metaclass=utils.Singleton):
         # home screen
         while True:
             self.draw_temperatures()
+            self.draw_weather()
+            self.draw_mode()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -89,8 +91,8 @@ class GUI(metaclass=utils.Singleton):
                     self.mouse_pos = pygame.mouse.get_pos()
 
                     try:
-                        if self.positions['off_button'].collidepoint(self.mouse_pos):
-                            self.stop(shutdown=True)
+                        if self.positions['power_toggle'].collidepoint(self.mouse_pos):
+                            self.thermostat.toggle_power()
                         if self.positions['settings_menu'].collidepoint(self.mouse_pos):
                             self.settings_view()
                         elif self.positions['low_temp_up'].collidepoint(self.mouse_pos):
@@ -146,25 +148,8 @@ class GUI(metaclass=utils.Singleton):
         self.screen.blit(self.images['up_arrow'], self.positions['high_temp_up'])
         self.screen.blit(self.images['down_arrow'], self.positions['high_temp_down'])
         self.screen.blit(self.images['settings_menu'], self.positions['settings_menu'])
-        self.screen.blit(self.images['off_button'], self.positions['off_button'])
-        Heating = self.fonts['heat_or_cool'].render("HEATING",1,self.heating_color)
-        Cooling = self.fonts['heat_or_cool'].render("COOLING",1,self.cooling_color)
-        Ext_Temp = self.fonts['external_temperature'].render(str(self.External_Temp) + "C",1,(0,0,0))
-        self.screen.blit(Ext_Temp, (185, 180)) 
-        if (self.HeatOrCool == 'h'):
-            self.screen.blit(Heating, (165,15))
-        else:
-            self.screen.blit(Cooling, (165,15))
-            
-        if (self.External_Temp_Icon == "rain"):
-            self.screen.blit(self.images['rain'], (250, 172))
-        if (self.External_Temp_Icon == "sun"):
-            self.screen.blit(self.images['sun'], (250, 172))
-        if (self.External_Temp_Icon == "wind"):
-            self.screen.blit(self.images['wind'], (250, 172))
-        if (self.External_Temp_Icon == "snow"):
-            self.screen.blit(self.images['snow'], (250, 172))                
-        
+        self.screen.blit(self.images['power_off'], self.positions['power_toggle'])
+
     def draw_temperatures(self, update=True):
         """
         Draws the temperature values on the home screen.
@@ -178,11 +163,11 @@ class GUI(metaclass=utils.Singleton):
 
         # render and draw text
         current_temperature = self.fonts['current_temperature'].render(
-            '{0}C'.format(self.thermostat.current_temperature), 1, (0,204,0))
+            '{0}C'.format(round(self.thermostat.current_temperature)), 1, self.TEMP_TEXT_COLOUR)
         low_temp = self.fonts['temperature_range'].render(
-            '{0}C'.format(self.thermostat.temperature_range_floor), 1, (0,204,0))
+            '{0}C'.format(round(self.thermostat.temperature_range_floor)), 1, self.TEMP_TEXT_COLOUR)
         high_temp = self.fonts['temperature_range'].render(
-            '{0}C'.format(self.thermostat.temperature_range_ceiling), 1, (0,204,0))
+            '{0}C'.format(round(self.thermostat.temperature_range_ceiling)), 1, self.TEMP_TEXT_COLOUR)
         self.screen.blit(current_temperature, self.positions['current_temperature'])
         self.screen.blit(low_temp, self.positions['low_temp'])
         self.screen.blit(high_temp, self.positions['high_temp'])
@@ -194,10 +179,45 @@ class GUI(metaclass=utils.Singleton):
                 self.positions['high_temp'],
             ])
 
+    def draw_weather(self, update=True):
+        """
+        Draws the external temperature value and status icon on the home screen.
+
+        :param update: if true, will update the display immediately
+        """
+        # reset
+        pygame.draw.rect(self.screen, self.BACKGROUND_COLOUR, self.positions['external_temp'])
+        pygame.draw.rect(self.screen, self.BACKGROUND_COLOUR, self.positions['weather_icon'])
+
+        # draw
+        external_temp = self.fonts['external_temperature'].render(
+            '{0}C'.format(round(self.thermostat.weather_thread.temperature)), 1, self.BLACK)
+        weather_icon = self.images.get(self.thermostat.weather_thread.status, self.images['clouds'])
+        self.screen.blit(external_temp, self.positions['external_temp'])
+        self.screen.blit(weather_icon, self.positions['weather_icon'])
+
+        if update is True:
+            pygame.display.update([
+                self.positions['external_temp'],
+                self.positions['weather_icon'],
+            ])
+
+    def draw_mode(self, update=True):
+        """
+        Draws the current mode on the home screen.
+
+        :param update: if true, will update the display immediately
+        """
+        pygame.draw.rect(self.screen, self.BACKGROUND_COLOUR, self.positions['mode'])
+        mode = self.fonts['mode'].render('{0} MODE'.format(self.thermostat.mode), 1, self.BLACK)
+        self.screen.blit(mode, self.positions['mode'])
+
+        if update is True:
+            pygame.display.update([self.positions['mode']])
+
     def draw_settings_screen(self):
         """
         Draws the static elements on the settings screen.
-        Assumes settings in Thermostat() is OrderedDict.
         """
         self.screen.fill(self.BACKGROUND_COLOUR)
         self.screen.blit(self.images['settings_title'], self.positions['settings_title'])
@@ -212,7 +232,6 @@ class GUI(metaclass=utils.Singleton):
     def draw_settings_values(self):
         """
         Draws the settings values.
-        Assumes settings in Thermostat() is OrderedDict.
         """
         pretty_settings = utils.prettify_settings(self.thermostat.settings)
         for i, value in enumerate(pretty_settings.values()):
